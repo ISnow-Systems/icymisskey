dbebb6eccb05de9f8d5055e68197ab5d9e3dc990
Hinata Hojo <hinata.hojo@isnow.xyz>
2023-08-19 09:43:37 +0900

    目に触れる変え忘れたところを変えるだけ(？)

--- packages/frontend/src/pages/about.vue
+++ packages/frontend/src/pages/about.vue
@@ -25,8 +25,8 @@ SPDX-License-Identifier: AGPL-3.0-only
 			<FormSection>
 				<div class="_gaps_m">
 					<MkKeyValue :copy="version">
-						<template #key>Misskey</template>
-						<template #value>{{ version }}</template>
+						<template #key>IcyMisskey</template>
+						<template #value>{{ version.replace(/[\d\.]+-icy\(([\d\.]+)\)/,"$1") }}</template>
 					</MkKeyValue>
 					<div v-html="i18n.t('poweredByMisskeyDescription', { name: instance.name ?? host })">
 					</div>
