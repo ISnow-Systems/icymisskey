72f788bdd666da48adc040062762296b2c8c890b
崎津 祥汰 <shota.sakitsu@virtlive.jp>
2024-02-08 02:10:17 +0900

    fix: パッチで壊れた要素の終了タグ関連を修復

--- packages/frontend/src/pages/about.vue
+++ packages/frontend/src/pages/about.vue
@@ -4,143 +4,144 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-<MkStickyContainer>
-	<template #header><MkPageHeader v-model:tab="tab" :actions="headerActions" :tabs="headerTabs"/></template>
-	<MkHorizontalSwipe v-model:tab="tab" :tabs="headerTabs">
-		<MkSpacer v-if="tab === 'overview'" :contentMax="600" :marginMin="20">
-			<div class="_gaps_m">
-				<div :class="$style.banner" :style="{ backgroundImage: `url(${ instance.bannerUrl })` }">
-					<div style="overflow: clip;">
-						<img :src="instance.iconUrl ?? instance.faviconUrl ?? '/favicon.ico'" alt="" :class="$style.bannerIcon"/>
-						<div :class="$style.bannerName">
-							<b>{{ instance.name ?? host }}</b>
+	<MkStickyContainer>
+		<template #header><MkPageHeader v-model:tab="tab" :actions="headerActions" :tabs="headerTabs"/></template>
+		<MkHorizontalSwipe v-model:tab="tab" :tabs="headerTabs">
+			<MkSpacer v-if="tab === 'overview'" :contentMax="600" :marginMin="20">
+				<div class="_gaps_m">
+					<div :class="$style.banner" :style="{ backgroundImage: `url(${ instance.bannerUrl })` }">
+						<div style="overflow: clip;">
+							<img :src="instance.iconUrl ?? instance.faviconUrl ?? '/favicon.ico'" alt="" :class="$style.bannerIcon"/>
+							<div :class="$style.bannerName">
+								<b>{{ instance.name ?? host }}</b>
+							</div>
 						</div>
 					</div>
-				</div>
 
-				<MkKeyValue>
-					<template #key>{{ i18n.ts.description }}</template>
-					<template #value><div v-html="instance.description"></div></template>
-				</MkKeyValue>
-
-			<FormSection>
-				<div class="_gaps_m">
-					<MkKeyValue :copy="version">
-						<template #key>IcyMisskey</template>
-						<template #value>{{ version.replace(/[\d\.]+-icy\(([\d\.]+)\)/,"$1") }}</template>
+					<MkKeyValue>
+						<template #key>{{ i18n.ts.description }}</template>
+						<template #value><div v-html="instance.description"></div></template>
 					</MkKeyValue>
-					<div v-html="i18n.t('poweredByMisskeyDescription', { name: instance.name ?? host })">
-					</div>
-					<FormLink to="/about-misskey">{{ i18n.ts.aboutMisskey }}</FormLink>
-				</div>
-			</FormSection>
 
-			<FormSection>
-				<div class="_gaps_m">
-					<FormSplit>
-						<MkKeyValue>
-							<template #key>{{ i18n.ts.administrator }}</template>
-							<template #value>{{ instance.maintainerName }}</template>
-						</MkKeyValue>
-						<MkKeyValue>
-							<template #key>{{ i18n.ts.contact }}</template>
-							<template #value>{{ instance.maintainerEmail }}</template>
-						</MkKeyValue>
-					</FormSplit>
-					<FormLink v-if="instance.impressumUrl" :to="instance.impressumUrl" external>{{ i18n.ts.impressum }}</FormLink>
-					<div class="_formLinks">
-						<MkFolder v-if="instance.serverRules.length > 0">
-							<template #label>{{ i18n.ts.serverRules }}</template>
-
-							<ol class="_gaps_s" :class="$style.rules">
-								<li v-for="item, index in instance.serverRules" :key="index" :class="$style.rule"><div :class="$style.ruleText" v-html="item"></div></li>
-							</ol>
-						</MkFolder>
-						<FormLink v-if="instance.tosUrl" :to="instance.tosUrl" external>{{ i18n.ts.termsOfService }}</FormLink>
-						<FormLink v-if="instance.privacyPolicyUrl" :to="instance.privacyPolicyUrl" external>{{ i18n.ts.privacyPolicy }}</FormLink>
-					</div>
-				</div>
-			</FormSection>
-
-			<FormSuspense :p="initStats">
-				<FormSection>
-					<div class="_gaps_m">
-						<MkKeyValue :copy="version">
-							<template #key>Misskey</template>
-							<template #value>{{ version }}</template>
-						</MkKeyValue>
-						<div v-html="i18n.tsx.poweredByMisskeyDescription({ name: instance.name ?? host })">
-						</div>
-						<FormLink to="/about-misskey">{{ i18n.ts.aboutMisskey }}</FormLink>
-					</div>
-				</FormSection>
-
-				<FormSection>
-					<div class="_gaps_m">
-						<FormSplit>
-							<MkKeyValue>
-								<template #key>{{ i18n.ts.administrator }}</template>
-								<template #value>{{ instance.maintainerName }}</template>
-							</MkKeyValue>
-							<MkKeyValue>
-								<template #key>{{ i18n.ts.contact }}</template>
-								<template #value>{{ instance.maintainerEmail }}</template>
-							</MkKeyValue>
-						</FormSplit>
-						<FormLink v-if="instance.impressumUrl" :to="instance.impressumUrl" external>{{ i18n.ts.impressum }}</FormLink>
-						<div class="_gaps_s">
-							<MkFolder v-if="instance.serverRules.length > 0">
-								<template #label>{{ i18n.ts.serverRules }}</template>
-
-								<ol class="_gaps_s" :class="$style.rules">
-									<li v-for="item, index in instance.serverRules" :key="index" :class="$style.rule"><div :class="$style.ruleText" v-html="item"></div></li>
-								</ol>
-							</MkFolder>
-							<FormLink v-if="instance.tosUrl" :to="instance.tosUrl" external>{{ i18n.ts.termsOfService }}</FormLink>
-							<FormLink v-if="instance.privacyPolicyUrl" :to="instance.privacyPolicyUrl" external>{{ i18n.ts.privacyPolicy }}</FormLink>
-						</div>
-					</div>
-				</FormSection>
-
-				<FormSuspense :p="initStats">
 					<FormSection>
-						<template #label>{{ i18n.ts.statistics }}</template>
-						<FormSplit>
-							<MkKeyValue>
-								<template #key>{{ i18n.ts.users }}</template>
-								<template #value>{{ number(stats.originalUsersCount) }}</template>
+						<div class="_gaps_m">
+							<MkKeyValue :copy="version">
+								<template #key>IcyMisskey</template>
+								<template #value>{{ version.replace(/[\d\.]+-icy\(([\d\.]+)\)/,"$1") }}</template>
 							</MkKeyValue>
-							<MkKeyValue>
-								<template #key>{{ i18n.ts.notes }}</template>
-								<template #value>{{ number(stats.originalNotesCount) }}</template>
-							</MkKeyValue>
-						</FormSplit>
+							<div v-html="i18n.t('poweredByMisskeyDescription', { name: instance.name ?? host })">
+							</div>
+							<FormLink to="/about-misskey">{{ i18n.ts.aboutMisskey }}</FormLink>
+						</div>
 					</FormSection>
-				</FormSuspense>
 
-				<FormSection>
-					<template #label>Well-known resources</template>
-					<div class="_gaps_s">
-						<FormLink :to="`/.well-known/host-meta`" external>host-meta</FormLink>
-						<FormLink :to="`/.well-known/host-meta.json`" external>host-meta.json</FormLink>
-						<FormLink :to="`/.well-known/nodeinfo`" external>nodeinfo</FormLink>
-						<FormLink :to="`/robots.txt`" external>robots.txt</FormLink>
-						<FormLink :to="`/manifest.json`" external>manifest.json</FormLink>
-					</div>
-				</FormSection>
-			</div>
-		</MkSpacer>
-		<MkSpacer v-else-if="tab === 'emojis'" :contentMax="1000" :marginMin="20">
-			<XEmojis/>
-		</MkSpacer>
-		<MkSpacer v-else-if="tab === 'federation'" :contentMax="1000" :marginMin="20">
-			<XFederation/>
-		</MkSpacer>
-		<MkSpacer v-else-if="tab === 'charts'" :contentMax="1000" :marginMin="20">
-			<MkInstanceStats/>
-		</MkSpacer>
-	</MkHorizontalSwipe>
-</MkStickyContainer>
+					<FormSection>
+						<div class="_gaps_m">
+							<FormSplit>
+								<MkKeyValue>
+									<template #key>{{ i18n.ts.administrator }}</template>
+									<template #value>{{ instance.maintainerName }}</template>
+								</MkKeyValue>
+								<MkKeyValue>
+									<template #key>{{ i18n.ts.contact }}</template>
+									<template #value>{{ instance.maintainerEmail }}</template>
+								</MkKeyValue>
+							</FormSplit>
+							<FormLink v-if="instance.impressumUrl" :to="instance.impressumUrl" external>{{ i18n.ts.impressum }}</FormLink>
+							<div class="_formLinks">
+								<MkFolder v-if="instance.serverRules.length > 0">
+									<template #label>{{ i18n.ts.serverRules }}</template>
+
+									<ol class="_gaps_s" :class="$style.rules">
+										<li v-for="item, index in instance.serverRules" :key="index" :class="$style.rule"><div :class="$style.ruleText" v-html="item"></div></li>
+									</ol>
+								</MkFolder>
+								<FormLink v-if="instance.tosUrl" :to="instance.tosUrl" external>{{ i18n.ts.termsOfService }}</FormLink>
+								<FormLink v-if="instance.privacyPolicyUrl" :to="instance.privacyPolicyUrl" external>{{ i18n.ts.privacyPolicy }}</FormLink>
+							</div>
+						</div>
+					</FormSection>
+
+					<FormSuspense :p="initStats">
+						<FormSection>
+							<div class="_gaps_m">
+								<MkKeyValue :copy="version">
+									<template #key>Misskey</template>
+									<template #value>{{ version }}</template>
+								</MkKeyValue>
+								<div v-html="i18n.tsx.poweredByMisskeyDescription({ name: instance.name ?? host })">
+								</div>
+								<FormLink to="/about-misskey">{{ i18n.ts.aboutMisskey }}</FormLink>
+							</div>
+						</FormSection>
+					</FormSuspense>
+
+					<FormSection>
+						<div class="_gaps_m">
+							<FormSplit>
+								<MkKeyValue>
+									<template #key>{{ i18n.ts.administrator }}</template>
+									<template #value>{{ instance.maintainerName }}</template>
+								</MkKeyValue>
+								<MkKeyValue>
+									<template #key>{{ i18n.ts.contact }}</template>
+									<template #value>{{ instance.maintainerEmail }}</template>
+								</MkKeyValue>
+							</FormSplit>
+							<FormLink v-if="instance.impressumUrl" :to="instance.impressumUrl" external>{{ i18n.ts.impressum }}</FormLink>
+							<div class="_gaps_s">
+								<MkFolder v-if="instance.serverRules.length > 0">
+									<template #label>{{ i18n.ts.serverRules }}</template>
+
+									<ol class="_gaps_s" :class="$style.rules">
+										<li v-for="item, index in instance.serverRules" :key="index" :class="$style.rule"><div :class="$style.ruleText" v-html="item"></div></li>
+									</ol>
+								</MkFolder>
+								<FormLink v-if="instance.tosUrl" :to="instance.tosUrl" external>{{ i18n.ts.termsOfService }}</FormLink>
+								<FormLink v-if="instance.privacyPolicyUrl" :to="instance.privacyPolicyUrl" external>{{ i18n.ts.privacyPolicy }}</FormLink>
+							</div>
+						</div>
+					</FormSection>
+
+					<FormSuspense :p="initStats">
+						<FormSection>
+							<template #label>{{ i18n.ts.statistics }}</template>
+							<FormSplit>
+								<MkKeyValue>
+									<template #key>{{ i18n.ts.users }}</template>
+									<template #value>{{ number(stats.originalUsersCount) }}</template>
+								</MkKeyValue>
+								<MkKeyValue>
+									<template #key>{{ i18n.ts.notes }}</template>
+									<template #value>{{ number(stats.originalNotesCount) }}</template>
+								</MkKeyValue>
+							</FormSplit>
+						</FormSection>
+					</FormSuspense>
+
+					<FormSection>
+						<template #label>Well-known resources</template>
+						<div class="_gaps_s">
+							<FormLink :to="`/.well-known/host-meta`" external>host-meta</FormLink>
+							<FormLink :to="`/.well-known/host-meta.json`" external>host-meta.json</FormLink>
+							<FormLink :to="`/.well-known/nodeinfo`" external>nodeinfo</FormLink>
+							<FormLink :to="`/robots.txt`" external>robots.txt</FormLink>
+							<FormLink :to="`/manifest.json`" external>manifest.json</FormLink>
+						</div>
+					</FormSection>
+				</div>
+			</MkSpacer>
+			<MkSpacer v-else-if="tab === 'emojis'" :contentMax="1000" :marginMin="20">
+				<XEmojis/>
+			</MkSpacer>
+			<MkSpacer v-else-if="tab === 'federation'" :contentMax="1000" :marginMin="20">
+				<XFederation/>
+			</MkSpacer>
+			<MkSpacer v-else-if="tab === 'charts'" :contentMax="1000" :marginMin="20">
+				<MkInstanceStats/>
+			</MkSpacer>
+		</MkHorizontalSwipe>
+	</MkStickyContainer>
 </template>
 
 <script lang="ts" setup>
